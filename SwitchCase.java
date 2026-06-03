ff3aa774ff267bd774cd5d9c1569a019e74e3b6f
Anurag Kumar Jha <januragkumar327@gmail.com>
2026-06-03 12:09:43 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,18 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        String  fruits = "Apple";
+       switch(fruits){
+
+        case "Apple":
+        System.out.println("Red fruits");
+        break;
+
+        case "Banana":
+        System.out.println("Yello fruits");
+        break;
+
+        default:
+         System.out.println("unknown fruits");
+       }
+    }
+}
