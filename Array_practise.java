540fb1cf21f4a6afc3a28e57fc4ffd2e0585d73f
Anurag Kumar Jha <januragkumar327@gmail.com>
2026-06-07 01:36:15 +0530

    Add files via upload

---
+++ Array_practise.java
@@ -0,0 +1,33 @@
+public class Array_practise {
+    public static void main(String[] args) {
+        System.out.println("Arrray prcatise we will do");
+        
+        float[] marks = {30.1f,30.2f,30.3f};
+        
+        float sum = 0;
+
+        for(float element: marks){
+            sum = sum + element;
+        }
+        
+        System.out.println("The value of Array is : "+ sum);
+
+        // float [] marks = {30.1f,30.2f,30.3f,30.4f};
+        // float sum = 34.7f;
+        
+        // for(float element: marks){
+        //     if(sum == element){
+        //       System.out.println("The value is same");
+        //     }
+        //     else{
+        //         System.out.println("The value are Diffrent");
+        //     }
+        // }
+
+        //  create a java progrma to add the two matrix of size 2 X 3 
+
+  
+
+
+    }
+}
